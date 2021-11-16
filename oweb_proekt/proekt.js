2127d529fc6409ed410482d834f7347bbdb289f2
anastasijast <anastasijast.9@gmail.com>
2021-11-17 00:07:54 +0100

    vtor_del

---
+++ oweb_proekt/proekt.js
@@ -0,0 +1,36 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+function sliki1(){
+    document.getElementById("pa1").style.display="block";
+    setTimeout(function(){
+        document.getElementById("pa1").style.display="none";}, 2000);
+}
+function sliki2(){
+    document.getElementById("pa2").style.display="block";
+    setTimeout(function(){
+        document.getElementById("pa2").style.display="none";}, 2000);
+}
+function sliki3(){
+    document.getElementById("pa3").style.display="block";
+    setTimeout(function(){
+        document.getElementById("pa3").style.display="none";}, 2000);
+}
+function sliki4(){
+    document.getElementById("pa4").style.display="block";
+    setTimeout(function(){
+        document.getElementById("pa4").style.display="none";}, 2000);
+}
+function sliki5(){
+    document.getElementById("pa5").style.display="block";
+    setTimeout(function(){
+        document.getElementById("pa5").style.display="none";}, 2000);
+}
+function sliki6(){
+    document.getElementById("pa6").style.display="block";
+    setTimeout(function(){
+        document.getElementById("pa6").style.display="none";}, 2000);
+}
