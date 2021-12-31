cca2453f17c2b592b6b483ff0b1cf391dfc09e30
anastasijast <anastasijast.9@gmail.com>
2021-12-31 15:22:00 +0100

    posleden_update

--- oweb_proekt/proekt.js
+++ oweb_proekt/proekt.js
@@ -1,3 +1,24 @@
+let p;
+function person(){
+    var pom1=getSavedValuePost("lajkovi1");
+    document.getElementById("lajk1").innerHTML=pom1;
+    pom1=getSavedValuePost("lajkovi2");
+    document.getElementById("lajk2").innerHTML=pom1;
+    pom1=getSavedValuePost("lajkovi3");
+    document.getElementById("lajk3").innerHTML=pom1;
+    pom1=getSavedValuePost("lajkovi4");
+    document.getElementById("lajk4").innerHTML=pom1;
+    pom1=getSavedValuePost("lajkovi5");
+    document.getElementById("lajk5").innerHTML=pom1;
+    pom1=getSavedValuePost("lajkovi6");
+    document.getElementById("lajk6").innerHTML=pom1;
+    p=prompt("Please enter your Email!");
+    if(p==null){
+        p="guest";
+    }
+    document.getElementById("first").innerHTML+=p;
+
+}
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -68,47 +89,74 @@ function like1(){
     var x=document.getElementById("srce1");
     x.className="fas fa-heart";
     setTimeout(function(){x.className="far fa-heart";}, 500);
-    document.getElementById("lajk1").innerHTML++;
+    var pom1=getSavedValuePost("lajkovi1");
+    pom1=++document.getElementById("lajk1").innerHTML;
+    localStorage.setItem("lajkovi1",pom1);
 }
 function like2(){
     var x=document.getElementById("srce2");
     x.className="fas fa-heart";
     setTimeout(function(){x.className="far fa-heart";}, 500);
-    document.getElementById("lajk2").innerHTML++;
+    var pom1=getSavedValuePost("lajkovi2");
+    pom1=++document.getElementById("lajk2").innerHTML;
+    localStorage.setItem("lajkovi2",pom1);
 }
 function like3(){
     var x=document.getElementById("srce3");
     x.className="fas fa-heart";
     setTimeout(function(){x.className="far fa-heart";}, 500);
-    document.getElementById("lajk3").innerHTML++;
+    var pom1=getSavedValuePost("lajkovi3");
+    pom1=++document.getElementById("lajk3").innerHTML;
+    localStorage.setItem("lajkovi3",pom1);
 }
 function like4(){
     var x=document.getElementById("srce4");
     x.className="fas fa-heart";
     setTimeout(function(){x.className="far fa-heart";}, 500);
-    document.getElementById("lajk4").innerHTML++;
+    var pom1=getSavedValuePost("lajkovi4");
+    pom1=++document.getElementById("lajk4").innerHTML;
+    localStorage.setItem("lajkovi4",pom1);
 }
 function like5(){
     var x=document.getElementById("srce5");
     x.className="fas fa-heart";
     setTimeout(function(){x.className="far fa-heart";}, 500);
-    document.getElementById("lajk5").innerHTML++;
+    var pom1=getSavedValuePost("lajkovi5");
+    pom1=++document.getElementById("lajk5").innerHTML;
+    localStorage.setItem("lajkovi5",pom1);
 }
 function like6(){
     var x=document.getElementById("srce6");
     x.className="fas fa-heart";
     setTimeout(function(){x.className="far fa-heart";}, 500);
-    document.getElementById("lajk6").innerHTML++;
+    var pom1=getSavedValuePost("lajkovi6");
+    pom1=++document.getElementById("lajk6").innerHTML;
+    localStorage.setItem("lajkovi6",pom1);
 }
+function getSavedValuePost(v) {
+    if (!localStorage.getItem(v)) {
+        localStorage.setItem(v, "");
+        return "";
+    }
+    return localStorage.getItem(v);
+  }
+  var pom1;
+  //ke ja koristam za localStorage
 function postiraj1(){
     var x=document.getElementById("tekst_kom1").value;
     const d = new Date();
-    document.getElementById("site_kom1").innerHTML+="<p class='cel'>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    var pom="<p class='cel'>"+p+":<br>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    document.getElementById("site_kom1").innerHTML+=pom;
+    pom1=getSavedValuePost("komentari");
+    pom1=pom1+pom;
+    localStorage.setItem("komentari",pom1);
 }
 function pokazi_kom1(){
     var x=document.getElementById("site_kom1");
     if(x.style.display==="none"){
         x.style.display="block";
+        var pom1=getSavedValuePost("komentari");
+        x.innerHTML+=pom1;
         document.getElementById("skrij_kom1").innerHTML="Hide Comments";
     }
     else{
@@ -119,12 +167,17 @@ function pokazi_kom1(){
 function postiraj2(){
     var x=document.getElementById("tekst_kom2").value;
     const d = new Date();
-    document.getElementById("site_kom2").innerHTML+="<p class='cel'>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
-}
+    var pom="<p class='cel'>"+p+":<br>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    document.getElementById("site_kom2").innerHTML+=pom;
+    pom1=getSavedValuePost("komentari2");
+    pom1=pom1+pom;
+    localStorage.setItem("komentari2",pom1);}
 function pokazi_kom2(){
     var x=document.getElementById("site_kom2");
     if(x.style.display==="none"){
         x.style.display="block";
+        var pom1=getSavedValuePost("komentari2");
+        x.innerHTML+=pom1;
         document.getElementById("skrij_kom2").innerHTML="Hide Comments";
     }
     else{
@@ -135,13 +188,18 @@ function pokazi_kom2(){
 function postiraj3(){
     var x=document.getElementById("tekst_kom3").value;
     const d = new Date();
-    document.getElementById("site_kom3").innerHTML+="<p class='cel'>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
-}
+    var pom="<p class='cel'>"+p+":<br>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    document.getElementById("site_kom3").innerHTML+=pom;
+    pom1=getSavedValuePost("komentari3");
+    pom1=pom1+pom;
+    localStorage.setItem("komentari3",pom1);}
 function pokazi_kom3(){
     var x=document.getElementById("site_kom3");
     if(x.style.display==="none"){
         x.style.display="block";
         document.getElementById("skrij_kom3").innerHTML="Hide Comments";
+        var pom1=getSavedValuePost("komentari3");
+        x.innerHTML+=pom1;
     }
     else{
         x.style.display="none";
@@ -151,12 +209,17 @@ function pokazi_kom3(){
 function postiraj4(){
     var x=document.getElementById("tekst_kom4").value;
     const d = new Date();
-    document.getElementById("site_kom4").innerHTML+="<p class='cel'>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
-}
+    var pom="<p class='cel'>"+p+":<br>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    document.getElementById("site_kom4").innerHTML+=pom;
+    pom1=getSavedValuePost("komentari4");
+    pom1=pom1+pom;
+    localStorage.setItem("komentari4",pom1);}
 function pokazi_kom4(){
     var x=document.getElementById("site_kom4");
     if(x.style.display==="none"){
         x.style.display="block";
+        var pom1=getSavedValuePost("komentari4");
+        x.innerHTML+=pom1;
         document.getElementById("skrij_kom4").innerHTML="Hide Comments";
     }
     else{
@@ -167,12 +230,17 @@ function pokazi_kom4(){
 function postiraj5(){
     var x=document.getElementById("tekst_kom5").value;
     const d = new Date();
-    document.getElementById("site_kom5").innerHTML+="<p class='cel'>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
-}
+    var pom="<p class='cel'>"+p+":<br>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    document.getElementById("site_kom5").innerHTML+=pom;
+    pom1=getSavedValuePost("komentari5");
+    pom1=pom1+pom;
+    localStorage.setItem("komentari5",pom1);}
 function pokazi_kom5(){
     var x=document.getElementById("site_kom5");
     if(x.style.display==="none"){
         x.style.display="block";
+        var pom1=getSavedValuePost("komentari5");
+        x.innerHTML+=pom1;
         document.getElementById("skrij_kom5").innerHTML="Hide Comments";
     }
     else{
@@ -183,12 +251,17 @@ function pokazi_kom5(){
 function postiraj6(){
     var x=document.getElementById("tekst_kom6").value;
     const d = new Date();
-    document.getElementById("site_kom6").innerHTML+="<p class='cel'>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
-}
+    var pom="<p class='cel'>"+p+":<br>"+x+"<p class='data'>"+d.toLocaleTimeString()+"</p>"+"</p>";
+    document.getElementById("site_kom6").innerHTML+=pom;
+    pom1=getSavedValuePost("komentari6");
+    pom1=pom1+pom;
+    localStorage.setItem("komentari6",pom1);}
 function pokazi_kom6(){
     var x=document.getElementById("site_kom6");
     if(x.style.display==="none"){
         x.style.display="block";
+        var pom1=getSavedValuePost("komentari6");
+        x.innerHTML+=pom1;
         document.getElementById("skrij_kom6").innerHTML="Hide Comments";
     }
     else{
@@ -311,33 +384,59 @@ function recept3(){
     }
 }
 function postiraj(){
-    var srci=document.getElementById("myFile").value;
-    const Niza=srci.split("\\");
+    /*
+    ova za postiranje na slika, ograniceno e da bide od 1 folder
+    ama moze da se stavaat poveke edna pod druga:)
+    */
+   var srci=document.getElementById("myFile").value;
+   const Niza=srci.split("\\");
     var source="./images/"+Niza[Niza.length-1];
    document.getElementById("blog_sliki").innerHTML+="<li>"+"<div class='blog_div' id='novo'>"+"<img src="+source+" class='sl2'>"+"</div>"+"</li>";
 }
 function postiraj_recept(){
     var x=document.getElementById("recept").value;
-    document.getElementById("novo").innerHTML+="<div class='recipe'>"+"<p style='background-color: white'>"+x+"</p>"+"</div>";
+    $("div.blog_div:last").append("<div class='recipe'>"+"<p style='background-color: white'>"+x+"</p>"+"</div>");
 }
 function pokazi_impresii(){
     var el=document.getElementById("impresii");
-    el.style.display="block";
-    var new_com=document.createElement("div");
-    el.appendChild(new_com);
-    new_com.className="prebaraj";
-    new_com.style.fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
-    new_com.style.padding="0 20px";
-    new_com.style.backgroundColor="white";
-    new_com.style.color="black";
-    new_com.style.borderRadius="15px";
+    // el.style.display="block";
     var ime=document.getElementById("ime").value;
     var email=document.getElementById("email").value;
     var tekst=document.getElementById("tekst").value;
     var ocenka=document.getElementById("ocenka").value;
-    new_com.innerHTML+="<p><b>Name:"+ime+"</b></p>"+"<p>Email:"+email+"</p>"+"<p>"+tekst+"</p>"+"<p> Rating:"+ocenka+"</p>";
+    // pom1=getSavedValuePost("oceni");
+    // el.innerHTML+=pom1;
+    if(p=="guest"){
+        var pom="<div class='new_com'>"+"<p><b>"+email+"</b></p>"+"<hr>"+"<p>Name:"+ime+"</p>"+"<p>"+tekst+"</p>"+"<p> Rating:"+ocenka+"</p>"+"</div>";
+        el.innerHTML+=pom;
+        pom1=pom1+pom;
+        localStorage.setItem("oceni",pom1);
+        //dokolku na pocetok ne pisal email, ke pise sega.
+    }
+    else{
+        var pom="<div class='new_com'>"+"<p><b>"+p+"</b></p>"+"<hr>"+"<p>Name:"+ime+"</p>"+"<p>"+tekst+"</p>"+"<p> Rating:"+ocenka+"</p>"+"</div>";
+        //ako na pocetok pisal email, vo poleto ke se vpise toj sto go pisal na pocetok a toa sto ke go pise vo poleto email ke se ignorira:)
+        el.innerHTML+=pom;
+        pom1=pom1+pom;
+        localStorage.setItem("oceni",pom1);
+    }
 
 }
 function hide_blog(){
-    document.getElementById("impresii").style.display="none";
+    var x=document.getElementById("impresii");
+    if(x.style.display==="none"){
+        x.style.display="block";
+        var pom1=getSavedValuePost("oceni");
+        x.innerHTML+=pom1;
+        document.getElementById("hide").innerHTML="Hide";
+        $("#search").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("div.new_com").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)    });
+            })
+    }
+    else{
+        x.style.display="none";
+        document.getElementById("hide").innerHTML="Show";
+    }
 }
